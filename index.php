399b26152b3570f9e7573f7a441eb254b2356216
sohelrana820 <runwithlight820>
2016-02-29 02:02:23 +0600

    Reformated code

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>SEO Moz Checker</title>
 
-    <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,200,300,700' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,200,300,700' rel='stylesheet'
+          type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -25,37 +26,37 @@
             color: #483D8B;
         }
 
-        .moz-checker-body .jumbotron{
+        .moz-checker-body .jumbotron {
             background: #483D8B !important;
             border-radius: 0px;
             padding: 100px;
         }
 
         .moz-checker-body .header,
-        .moz-checker-body  .moz-report,
-        .moz-checker-body  .footer {
+        .moz-checker-body .moz-report,
+        .moz-checker-body .footer {
             padding-right: 15px;
             padding-left: 15px;
         }
 
-        .moz-checker-body  .header {
+        .moz-checker-body .header {
             padding-bottom: 20px;
             border-bottom: 1px solid #e5e5e5;
         }
 
-        .moz-checker-body  .header h3 {
+        .moz-checker-body .header h3 {
             margin-top: 0;
             margin-bottom: 0;
             line-height: 40px;
         }
 
-        .moz-checker-body  .footer {
+        .moz-checker-body .footer {
             padding-top: 19px;
             color: #777;
             border-top: 1px solid #e5e5e5;
         }
 
-        .moz-checker-body .form-control{
+        .moz-checker-body .form-control {
             border: 0px;
             background: none;
             border-bottom: 2px solid #FFF;
@@ -86,8 +87,7 @@
         }
 
         .moz-checker-body .panel,
-        .moz-checker-body .alert
-        {
+        .moz-checker-body .alert {
             border-radius: 0px;
         }
 
@@ -103,45 +103,46 @@
         }
 
         @media (min-width: 768px) {
-            .moz-checker-body  .container {
+            .moz-checker-body .container {
                 max-width: 730px;
             }
         }
-        .moz-checker-body  .container-narrow > hr {
+
+        .moz-checker-body .container-narrow > hr {
             margin: 30px 0;
         }
 
-        .moz-checker-body  .jumbotron {
+        .moz-checker-body .jumbotron {
             text-align: center;
             border-bottom: 1px solid #e5e5e5;
         }
 
-        .moz-checker-body  .jumbotron .btn {
+        .moz-checker-body .jumbotron .btn {
             padding: 14px 24px;
             font-size: 21px;
         }
 
-        .moz-checker-body  .moz-report {
+        .moz-checker-body .moz-report {
             margin: 40px 0;
         }
 
-        .moz-checker-body  .moz-report p + h4 {
+        .moz-checker-body .moz-report p + h4 {
             margin-top: 28px;
         }
 
         @media screen and (min-width: 768px) {
-            .moz-checker-body  .header,
-            .moz-checker-body  .moz-report,
-            .moz-checker-body  .footer {
+            .moz-checker-body .header,
+            .moz-checker-body .moz-report,
+            .moz-checker-body .footer {
                 padding-right: 0;
                 padding-left: 0;
             }
 
-            .moz-checker-body  .header {
+            .moz-checker-body .header {
                 margin-bottom: 30px;
             }
 
-            .moz-checker-body  .jumbotron {
+            .moz-checker-body .jumbotron {
                 border-bottom: 0;
             }
         }
@@ -154,10 +155,7 @@
         <h2 class="title">Check your SEO MOZ</h2>
     </div>
 
-    <div id="message">
-
-    </div>
-
+    <div id="message"></div>
     <div class="jumbotron">
         <div class="form-group">
             <input name="url" type="text" id="url" class="form-control" placeholder="Website url">
@@ -170,51 +168,49 @@
         <div class="row">
             <div class="col-lg-6">
                 <div class="panel panel-primary">
-                    <div class="panel-heading"> <h3 class="panel-title">PAGE AUTHORITY (PA)</h3> </div>
-                    <div class="panel-body"> <span id="upa"></span></div>
+                    <div class="panel-heading"><h3 class="panel-title">PAGE AUTHORITY (PA)</h3></div>
+                    <div class="panel-body"><span id="upa"></span></div>
                 </div>
             </div>
             <div class="col-lg-6">
                 <div class="panel panel-success">
-                    <div class="panel-heading"> <h3 class="panel-title">DOMAIN AUTHORITY (DA)</h3> </div>
-                    <div class="panel-body"> <span id="pda"></span></div>
+                    <div class="panel-heading"><h3 class="panel-title">DOMAIN AUTHORITY (DA)</h3></div>
+                    <div class="panel-body"><span id="pda"></span></div>
                 </div>
             </div>
             <div class="col-lg-6">
                 <div class="panel panel-info">
-                    <div class="panel-heading"> <h3 class="panel-title">MOZ RANK</h3> </div>
-                    <div class="panel-body"> <span id="umrp"></span></div>
+                    <div class="panel-heading"><h3 class="panel-title">MOZ RANK</h3></div>
+                    <div class="panel-body"><span id="umrp"></span></div>
                 </div>
             </div>
             <div class="col-lg-6">
                 <div class="panel panel-warning">
-                    <div class="panel-heading"> <h3 class="panel-title">BACKLINKS</h3> </div>
-                    <div class="panel-body"> <span id="uid"></span></div>
+                    <div class="panel-heading"><h3 class="panel-title">BACKLINKS</h3></div>
+                    <div class="panel-body"><span id="uid"></span></div>
                 </div>
             </div>
             <div class="clearfix"></div>
         </div>
     </div>
-
     <footer class="footer">
         <p>&copy; 2016 Company, Inc.</p>
     </footer>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script>
-        $(function() {
-            $('#checkBtn').click(function(){
+        $(function () {
+            $('#checkBtn').click(function () {
                 $('.moz-report').hide();
                 var url = $('#url').val();
                 $('#url').val('');
-                $.ajax( {
+                $.ajax({
                     type: "POST",
-                    url:'moz.php',
+                    url: 'moz.php',
                     data: {url: url},
-                    success:function(response) {
-                        if(response == 1)
-                        {
+                    success: function (response) {
+                        if (response == 1) {
                             $('#message').html('' +
                                 '<div class="alert alert-danger alert-dismissible fade in text-uppercase text-center" role="alert"> ' +
                                 '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>' +
@@ -222,8 +218,7 @@
                                 '</div>'
                             ).fadeIn();
                         }
-                        else if(response == 2)
-                        {
+                        else if (response == 2) {
                             $('#message').html('' +
                                 '<div class="alert alert-danger alert-dismissible fade in text-uppercase text-center" role="alert"> ' +
                                 '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>' +
@@ -231,11 +226,11 @@
                                 '</div>'
                             ).fadeIn();
                         }
-                        else{
+                        else {
                             response = jQuery.parseJSON(response);
-                            $('#upa').html(response.upa+'/100');
-                            $('#pda').html(response.pda+'/100');
-                            $('#umrp').html(response.umrp+'/10');
+                            $('#upa').html(response.upa + '/100');
+                            $('#pda').html(response.pda + '/100');
+                            $('#umrp').html(response.umrp + '/10');
                             $('#uid').html(response.uid);
                             $('.moz-report').fadeIn();
                         }
